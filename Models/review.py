d1cedee3181869a5b91f62533b115b8ef22ad799
Charlie <carlos.esteban19@outlook.com>
2024-07-05 20:39:57 -0400

    sql aclhemy models

--- Models/review.py
+++ Models/review.py
@@ -1,10 +1,19 @@
 import uuid
 from datetime import datetime
 from Persistence.data_manager import DataManager
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 
 class Review(DataManager):
     """class that defines a review"""
+    __tablename__ = 'reviews'
+
+    place_id = db.Column(db.String, db.ForeignKey('places.id'), nullable=False)
+    user_id = db.Column(db.String, db.ForeignKey('users.id'), nullable=False)
+    comment = db.Column(db.String, nullable=False)
+    rating = db.Column(db.Float, nullable=False)
 
     def __init__(self, user_id: str, place_id, comment: str, rating: int):
         """initialize a review"""
@@ -15,61 +24,6 @@ class Review(DataManager):
         self.__place_id = place_id
         self.__comment = comment
         self.__rating = rating
-
-    @property
-    def id(self):
-        """id getter"""
-        return self.__id
-
-    @property
-    def created_at(self):
-        """creation datetime getter"""
-        return self.__created_at
-
-    @property
-    def updated_at(self):
-        """last update datetime getter"""
-        return self.__updated_at
-
-    @property
-    def user_id(self):
-        """user id getter"""
-        return self.__user_id
-
-    @property
-    def place_id(self):
-        """place id getter"""
-        return self.__place_id
-
-    @property
-    def comment(self):
-        """comment getter"""
-        return self.__comment
-
-    @comment.setter
-    def comment(self, comment: str):
-        """comment setter"""
-        if type(comment) is not str:
-            raise TypeError("comment must be a valid string")
-        if not comment or len(comment.strip()) == 0:
-            raise ValueError("comment cannot be empty")
-        self.__comment = comment
-        self.__updated_at = datetime.now().strftime("%B/%d/%Y %I:%M:%S %p")
-
-    @property
-    def rating(self):
-        """rating getter"""
-        return self.__rating
-
-    @rating.setter
-    def rating(self, rating: int):
-        """rating setter"""
-        if type(rating) is not int:
-            raise TypeError("rating must be an integer")
-        if rating < 1 or rating > 5:
-            raise ValueError("rating must be between 1 and 5")
-        self.__rating = rating
-        self.__updated_at = datetime.now().strftime("%B/%d/%Y %I:%M:%S %p")
 
     def to_dict(self):
         """convert review to dict"""
@@ -82,7 +36,6 @@ class Review(DataManager):
             "comment": self.__comment,
             "rating": self.__rating,
         }
-
 
     @classmethod
     def from_dict(cls, data):
